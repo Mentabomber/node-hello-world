c149d6fc2e0015a16b1cceb632e53454873530c8
Mentabomber <mentae_mentology@libero.it>
2023-11-07 17:22:41 +0100

    added function to only see the sentences once until they are all been displayed

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const port = process.env.PORT || 3000;
 const phrases = [
     "He who seeks shall find",
     "The most effective way to do it, is to do it",
-    // "You don't have to be perfect to be amazing",
-    // "Make each day your masterpiece",
-    // "Every moment is a fresh beginning",
-    // "A winner is a dreamer who never gives up",
-    // "Enthusiasm moves the world",
-    // "You cannot have a positive life with a negative mind",
-    // "Be yourself. Everyone else is already taken",
-    // "If you really believe in something, it's important"
+    "You don't have to be perfect to be amazing",
+    "Make each day your masterpiece",
+    "Every moment is a fresh beginning",
+    "A winner is a dreamer who never gives up",
+    "Enthusiasm moves the world",
+    "You cannot have a positive life with a negative mind",
+    "Be yourself. Everyone else is already taken",
+    "If you really believe in something, it's important"
 ];
+
+
 
 let lastPhraseNumber;
 
@@ -34,6 +36,25 @@ function postRandomPhrase(phrases) {
     return phrases[phraseNumber];
 }
 
+let sentences = [];
+
+function postRandomSentence(phrases){
+    let sentenceNumber;
+    do {
+        sentenceNumber = Math.floor(Math.random() * phrases.length);
+        console.log(sentenceNumber, "sentenceNumber");
+        if (sentences.length === phrases.length) {
+            console.log("tutte le frasi sono state visualizzate!");
+            sentences = [];
+            return `<h2>tutte le frasi sono state visualizzate!</h2>`
+        }
+    } while (sentences.includes(sentenceNumber));
+
+    sentences.push(sentenceNumber);
+    console.log(sentences);
+    return phrases[sentenceNumber];
+}
+
 function htmlResponse(res, content) {
     res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
     res.end(content);
@@ -42,7 +63,9 @@ function htmlResponse(res, content) {
 const server = http.createServer(function (req, res) {
     if(req.url === "/favicon.ico") res.writeHead(404).end();
     const selectedPhrase = postRandomPhrase(phrases);
-    htmlResponse(res, `<h2>${selectedPhrase}</h2>`);
+    const selectedSentence = postRandomSentence(phrases);
+    htmlResponse(res, `<h2>${selectedPhrase}</h2> <br> 
+                        <h2>${selectedSentence}</h2>`);
 });
 
 server.listen(port, function () {
